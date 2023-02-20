8d16827f4cfb4de895535899a14cef6a62f6cfc8
Shachar Langbeheim <shachlan@amazon.com>
2023-02-20 20:08:57 +0000

    Revert "socket in between"
    This reverts commit 3277127f37ec232881a09e4027ee00fe517cc374.

--- babushka-core/src/lib.rs
+++ babushka-core/src/lib.rs
@@ -2,8 +2,7 @@ mod fake_multiplexer;
 /// Contains information that determines how the request and response headers are shaped.
 pub mod headers;
 mod rotating_buffer;
-mod socket_like;
 mod socket_like_listener;
 mod socket_listener_impl;
-pub use socket_like::*;
+pub use socket_like_listener::*;
 pub use socket_listener_impl::*;
