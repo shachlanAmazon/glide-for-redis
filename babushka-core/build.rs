a61f8a101609390b8d0cce35b62b2845dc4ec833
Shachar Langbeheim <shachlan@amazon.com>
2023-06-09 13:03:32 +0000

    temp

--- babushka-core/build.rs
+++ babushka-core/build.rs
@@ -1,9 +1,11 @@
 fn main() {
+    let customization_options = protobuf_codegen::Customize::default().lite_runtime(false);
     protobuf_codegen::Codegen::new()
         .cargo_out_dir("protobuf")
         .include("src")
         .input("src/protobuf/redis_request.proto")
         .input("src/protobuf/response.proto")
         .input("src/protobuf/connection_request.proto")
+        .customize(customization_options)
         .run_from_script();
 }
